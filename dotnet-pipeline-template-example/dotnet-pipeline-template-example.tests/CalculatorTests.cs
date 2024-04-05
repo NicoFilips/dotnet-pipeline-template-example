ea553b493bb683a052c65ef8c14b747fb73013b2
Nico Filips <nico.filips@de.sii.group>
2024-04-05 10:21:59 +0200

    tests: add Calculator Unittests

---
+++ dotnet-pipeline-template-example/dotnet-pipeline-template-example.tests/CalculatorTests.cs
@@ -0,0 +1,74 @@
+using dotnet_pipeline_template_example.domain;
+using NUnit;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace dotnet_pipeline_template_example.tests;
+
+public class CalculatorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Add_whenResultisCorrect_returnTrue()
+    {
+        // Arrange
+        var calculator = new Calculator();
+        var a = 1;
+        var b = 2;
+
+        // Act
+        var result = calculator.Add(a, b);
+
+        // Assert
+        result.Should().Be(3);
+    }
+    
+    [Test]
+    public void Subtract_whenResultisCorrect_returnTrue()
+    {
+        // Arrange
+        var calculator = new Calculator();
+        var a = 1;
+        var b = 2;
+
+        // Act
+        var result = calculator.Subtract(a, b);
+
+        // Assert
+        result.Should().Be(-1);
+    }
+    
+    [Test]
+    public void Multiply_whenResultisCorrect_returnTrue()
+    {
+        // Arrange
+        var calculator = new Calculator();
+        var a = 1;
+        var b = 2;
+
+        // Act
+        var result = calculator.Multiply(a, b);
+
+        // Assert
+        result.Should().Be(2);
+    }
+    
+    [Test]
+    public void Divide_whenResultisCorrect_returnTrue()
+    {
+        // Arrange
+        var calculator = new Calculator();
+        var a = 4;
+        var b = 2;
+
+        // Act
+        var result = calculator.Divide(a, b);
+
+        // Assert
+        result.Should().Be(2);
+    }
+}
